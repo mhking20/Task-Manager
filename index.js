1e10457de50027057baba2ab59b3b900f7447faf
mhking20 <www.taimoorking20@gmail.com>
2023-02-10 15:08:14 +0500

    hh

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
+const bodyparser = require("body-parser");
+app.use(bodyparser.json());
+
 app.use(express.static("./public"));
 app.use(express.json());
 
